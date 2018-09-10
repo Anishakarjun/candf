39560c2e3665e73a550538589082cbd60e39b720
Anisha KA <anisha.ka@valintechnologies.in>
2018-09-10 10:06:28 +0530

    first commit

---
+++ wp-content/themes/carl-friedrich/single-property.php
@@ -0,0 +1,341 @@
+<?php get_header();?>
+<!--slider -->
+<div class="slider-property">
+      <div  id="example-19" class="ma5slider anim-horizontal horizontal-dots horizontal-navs center-dots inside-dots">
+        <div class="slides">
+            <a href="#slide-1"><img src="img/img-1.jpg" alt=""></a>
+            <a href="#slide-2"><img src="img/img-2.jpg" alt=""></a>
+            <a href="#slide-3"><img src="img/img-3.jpg" alt=""></a>
+        </div>
+    </div>
+      </div>
+
+
+      <!-- proepry details -->
+      <div class="container">
+<div class="col-md-8 col-xs-12 col-sm-12  wow fadeInRight">
+<div class="property-details">
+
+<h1>Short description of the property</h1>
+<h2>Prague, Czech Republic</h2>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+<!-- map -->
+  <div class="slider-map">
+  <div class="row">
+    <div id="slider-map" class="owl-carousel">
+  
+  <div class="item">    
+<img class="img-responsive" src="img/floor.jpg"/>
+</div>
+
+  <div class="item">    
+<img class="img-responsive" src="img/h1.jpg"/>
+</div>
+
+  <div class="item">    
+<img class="img-responsive" src="img/floor.jpg"/>
+</div>
+          
+ 
+
+
+</div>
+</div>
+
+          <a class="grayl prev">&#60;</a>
+    <a class="grayr next">&#62;</a>
+
+</div>
+
+<!-- map end -->
+<p class="two-coloumn">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
+
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores.
+</p>
+
+
+
+      </div>
+ </div>
+
+
+      <div class="col-md-4 col-xs-12 col-sm-12  wow fadeInLeft">
+<div class="specifications">
+<h1>Specifications</h1>
+<div class="clearfix"></div>
+
+<table cellpadding="0" cellspacing="0" width="100%" >
+<tr>
+<td>
+<h1>
+Price
+</h1>
+</td>
+
+<td>
+<h2>9.309.672,00 Kč</h2>
+</td>
+</tr>
+
+<tr>
+<td>
+<h1>
+Square Meters
+</h1>
+</td>
+
+<td>
+<h2>120 m2 </h2>
+</td>
+</tr>
+
+<tr>
+<td>
+<h1>
+Balcony
+</h1>
+</td>
+
+<td>
+<h2><span><i class="fa fa-check"></i> </span></h2>
+</td>
+</tr>
+
+<tr>
+<td>
+<h1>
+Parking
+</h1>
+</td>
+
+<td>
+<h2><span><i class="fa fa-check"></i> </span></h2>
+</td>
+</tr>
+
+
+<tr>
+<td>
+<h1>
+Basement
+</h1>
+</td>
+
+<td>
+<h2><span><i class="fa fa-check"></i> </span></h2>
+</td>
+</tr>
+</table>
+</div>
+
+<!-- downlalod buttern -->
+
+<a href="#"><div class="downlaodb"><i class="fa fa-download"></i> Download the Broucher </div></a>
+
+<div class="clearfix"> </div>
+
+
+<div class="mail">
+<h1>Contact Us</h1>
+<div class="mailform">
+<form action="#">
+<input class="form" type="text" name="name" placeholder="Name (required)">
+<div class="clearfix"> </div>
+
+<input class="form" type="email" name="email" placeholder="Email (required)">
+<div class="clearfix"> </div>
+
+<textarea rows="7" class="form" type="text" name="text">Hello, I am interested in finding out more info about this property</textarea>
+<div class="clearfix"> </div>
+
+<button type="submit" class="submit">Send  <i class="fa fa-long-arrow-right"></i></button>
+</form>
+</div>
+</div>
+
+      </div>
+     
+      </div>
+
+   <!-- gallery -->
+
+    <div class="container wow fadeInUp">
+    <div class="">
+
+  
+  
+<div class="col-md-4 col-xs-12">
+<a onclick="openModal();currentSlide(1)">
+    <div>
+       
+  <div class="sliding-img-pro">
+<img class="img-responsive" src="img/h1.jpg"/>
+<div id="overlay5-pro">
+<div class="img-title-pro">
++5 More Photos
+</div>
+<img src="img/cf-mark.png" class="img-icon-pro-p"/>
+</div>
+</div>
+          
+    </div>
+    </a>
+    </div>
+
+    <div class="col-md-4 col-xs-12">
+    <a onclick="openModal();currentSlide(2)">
+    <div>
+    
+  <div class="sliding-img-pro">
+<img class="img-responsive" src="img/h2.jpg"/>
+<div id="overlay5-pro">
+<div class="img-title-pro">
++5 More Photos
+</div>
+<img src="img/cf-mark.png" class="img-icon-pro-p"/>
+</div>
+</a>
+</div>
+            
+    </div>
+    </div>
+
+    <div class="col-md-4 col-xs-12">
+    <a onclick="openModal();currentSlide(3)">
+    <div>
+      
+  <div class="sliding-img-pro">
+<img class="img-responsive" src="img/h2.jpg"/>
+<div id="overlay5-pro">
+<div class="img-title-pro">
++5 More Photos
+</div>
+<img src="img/cf-mark.png" class="img-icon-pro-p"/>
+</div>
+</div>
+          
+    </div>
+    </a>
+    </div>
+
+        <div class="col-md-4 col-xs-12">
+        <a onclick="openModal();currentSlide(4)">
+    <div>
+        
+  <div class="sliding-img-pro">
+<img class="img-responsive" src="img/h1.jpg"/>
+<div id="overlay5-pro">
+<div class="img-title-pro">
++5 More Photos
+</div>
+<img src="img/cf-mark.png" class="img-icon-pro-p"/>
+</div>
+</div>
+           
+    </div>
+    </a>
+    </div>
+
+
+        <div class="col-md-4 col-xs-12">
+        <a onclick="openModal();currentSlide(5)">
+    <div>
+      
+  <div class="sliding-img-pro">
+<img class="img-responsive" src="img/h1.jpg"/>
+<div id="overlay5-pro">
+<div class="img-title-pro">
++5 More Photos
+</div>
+<img src="img/cf-mark.png"  class="img-icon-pro-p"/>
+</div>
+</div>
+          
+    </div>
+    </a>
+    </div>
+
+
+        <div class="col-md-4 col-xs-12">
+        <a onclick="openModal();currentSlide(6)">
+    <div>
+       
+  <div class="sliding-img-pro">
+<img class="img-responsive" src="img/h2.jpg"/>
+<div id="overlay5-pro">
+<div class="img-title-pro">
++5 More Photos
+</div>
+<img src="img/cf-mark.png" class="img-icon-pro-p"/>
+</div>
+</div>
+          
+    </div>
+    </a>
+    </div>
+    </div>
+    </div>
+
+
+    <!-- The Modal/Lightbox -->
+<div id="myModal" class="modal">
+  <span class="close1 cursor" onclick="closeModal()">&times;</span>
+  <div class="modal-content">
+
+    <div class="mySlides">
+   
+      <img src="img/h1.jpg" style="width:100%">
+    </div>
+
+    <div class="mySlides">
+    
+      <img src="img/h2.jpg" style="width:100%">
+    </div>
+
+    <div class="mySlides">
+      
+      <img src="img/h2.jpg" style="width:100%">
+    </div>
+
+    <div class="mySlides">
+     
+      <img src="img/h1.jpg" style="width:100%">
+    </div>
+
+      <div class="mySlides">
+    
+      <img src="img/h1.jpg" style="width:100%">
+    </div>
+
+      <div class="mySlides">
+      
+      <img src="img/h2.jpg" style="width:100%">
+    </div>
+
+
+
+    <!-- Next/previous controls -->
+    <a class="prev1" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="next1" onclick="plusSlides(1)">&#10095;</a>
+
+    <!-- Caption text -->
+    
+
+    </div>
+    </div>
+
+
+    <!-- map -->
+<div class="map wow zoomInUp">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2560.405018521703!2d14.419236115209843!3d50.07870342196274!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470b94f2510a45c7%3A0x590f01dd7b70f28a!2sTRIUMPH+%C4%8Cesk%C3%A1+republika+s.r.o.!5e0!3m2!1sen!2sin!4v1531634637196" width="100%" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+    </div>
+
+
+
+
+     
+<?php get_footer(); ?>
